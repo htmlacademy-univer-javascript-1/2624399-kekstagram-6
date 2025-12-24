101dc3ad2a48e8576ac06965c115e7f27a1aa799
Lucik-228 <luciksnusik228@gmail.com>
2025-12-25 00:41:55 +0500

    feat(api.js, main.js, util.js): implement data fetching and alert display functionality, enhance utility functions

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
-import { getPhotos } from './data.js';
 import { renderThumbnails } from './thumbnail.js';
+// import { initFilter } from './filter.js';
+import { getData } from './api.js';
+import { showAlert } from './util.js';
 import './form.js';
 
-renderThumbnails(getPhotos());
+getData(
+  (loadedPictures) => {
+    renderThumbnails(loadedPictures);
+    initFilter(loadedPictures, renderThumbnails);
+  },
+  (err) => showAlert(err)
+);
--- js/util.js
+++ js/util.js
@@ -15,4 +15,44 @@ const createIdGenerator = () => {
   };
 };
 
-export { getRandomInteger, getRandomArrayElement, createIdGenerator };
+function debounce (callback, timeoutDelay = 500) {
+  let timeoutId;
+  return (...rest) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
+}
+
+function throttle (callback, delayBetweenFrames) {
+  let lastTime = 0;
+  return (...rest) => {
+    const now = new Date();
+    if (now - lastTime >= delayBetweenFrames) {
+      callback.apply(this, rest);
+      lastTime = now;
+    }
+  };
+}
+
+const showAlert = (message) => {
+  const alertContainer = document.createElement('div');
+  alertContainer.style.zIndex = '100';
+  alertContainer.style.position = 'absolute';
+  alertContainer.style.left = '0';
+  alertContainer.style.top = '0';
+  alertContainer.style.right = '0';
+  alertContainer.style.padding = '10px 3px';
+  alertContainer.style.fontSize = '30px';
+  alertContainer.style.textAlign = 'center';
+  alertContainer.style.backgroundColor = 'red';
+
+  alertContainer.textContent = message;
+
+  document.body.append(alertContainer);
+
+  setTimeout(() => {
+    alertContainer.remove();
+  }, 5000);
+};
+
+export { getRandomInteger, getRandomArrayElement, createIdGenerator, debounce, throttle, showAlert };
